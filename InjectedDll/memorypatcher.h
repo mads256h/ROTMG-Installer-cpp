c45b21357a3b4fa7ea8c2f38f5c3ed151bb97ccf
Mads Mogensen <lort256c@gmail.com>
2018-02-25 01:38:47 +0100

    Patch memory so that JPEXS cant pull swf

---
+++ InjectedDll/memorypatcher.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <ostream>
+#include <iostream>
+#include <Windows.h>
+
+void MemoryPatcher()
+{
+	HANDLE hProcess = GetCurrentProcess();
+
+
+	const CHAR overwrite[] = { 'X', 'X', 'X' };
+
+	unsigned long long addr = 0x008E0000;
+	unsigned long long possibleAddr = addr;
+	int state = 0;
+	int error = 0;
+	do
+	{
+		SetLastError(0);
+
+		CHAR buffer1[512];
+
+		ReadProcessMemory(hProcess, (void*)addr, &buffer1, sizeof(buffer1), NULL);
+
+		error = GetLastError();
+
+		for (size_t i = 0; i < 512; i++)
+		{
+			CHAR buffer = buffer1[i];
+
+			switch (state)
+			{
+			case 0:
+				if (buffer == 'F')
+				{
+					possibleAddr = addr + i;
+					state = 1;
+				}
+				else
+				{
+					state = 0;
+				}
+				break;
+
+			case 1:
+				if (buffer == 'W')
+				{
+					state = 2;
+				}
+				else
+				{
+					state = 0;
+				}
+				break;
+
+			case 2:
+				if (buffer == 'S')
+				{
+					WriteProcessMemory(hProcess, (void*)possibleAddr, overwrite, sizeof(overwrite), NULL);
+					std::cout << GetLastError() << std::endl;
+					std::cout << "Overwrote this: " << std::hex << possibleAddr << std::dec << std::endl;
+					state = 0;
+				}
+				state = 0;
+
+			default:
+				state = 0;
+				break;
+			}
+
+		}
+
+		addr += sizeof(buffer1);
+
+	} while (error == 0 || error == 299 && addr <= 0x20000000);
+}
